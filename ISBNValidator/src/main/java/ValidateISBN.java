f6d766282bd4f4c105af03ea1b7a6e086c41e67e
Emre YILDIZ <emreyldz188@gmail.com>
2022-11-07 14:10:04 +0300

    Add files via upload

---
+++ ISBNValidator/src/main/java/ValidateISBN.java
@@ -0,0 +1,5 @@
+/**
+ * @author Emre YILDIZ
+ */
+public class ValidateISBN {
+}
---
+++ ISBNValidator/src/test/java/SampleTests.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * @author Emre YILDIZ
+ */
+public class SampleTests {
+    @Test
+    void name()
+    {
+        fail("sa");
+    }
+}
